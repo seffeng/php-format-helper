79bebc97fe9a9008cae0479321c98b0837c8a4b2
seffeng <seffeng@sina.cn>
2023-06-06 16:03:18 +0800

    update.

--- src/Traits/FormatTrait.php
+++ src/Traits/FormatTrait.php
@@ -11,11 +11,11 @@ Trait FormatTrait
      *
      * @author zxf
      * @date   2020年5月26日
-     * @param int $size 被转换的数字 字节[Byte]
-     * @param int $decim 小数点位数 default[2]2位小数点
-     * @param int $units 进制单位大小 default[1024]
-     * @param int $valCrf 取整类型 default[0]0-四舍五入,1-向下取整,2-向上取整
-     * @param int $pad 连接符
+     * @param integer $size 被转换的数字 字节[Byte]
+     * @param integer $decim 小数点位数 default[2]2位小数点
+     * @param integer $units 进制单位大小 default[1024]
+     * @param integer $valCrf 取整类型 default[0]0-四舍五入,1-向下取整,2-向上取整
+     * @param integer $pad 连接符
      * @return string
      */
     public static function sizeFormat(int $size, int $decim = 2, int $units = 1024, int $valCrf = 0, string $pad = ' ')
